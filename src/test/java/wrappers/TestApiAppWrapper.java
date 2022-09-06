0f9a6d836713ea68f23cd736404b22f624299adb
Dmitriy Tonkushin <DTonkushin@alfabank.ru>
2022-09-06 14:56:00 +0500

    Build DockerImage from Dockerfile

--- src/test/java/wrappers/TestApiAppWrapper.java
+++ src/test/java/wrappers/TestApiAppWrapper.java
@@ -1,15 +1,24 @@
 package wrappers;
 
 import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.images.builder.ImageFromDockerfile;
 import org.testcontainers.images.builder.dockerfile.DockerfileBuilder;
 
 public class TestApiAppWrapper  extends GenericContainer<TestApiAppWrapper> {
     public static final String SERVICE_NAME = "test-api-app";
 
-    private String buildDockerfile() {
+    private static ImageFromDockerfile buildImage(String serviceName) {
         String imageName = "arm64v8/openjdk:11-jdk";
         String jarFileName = "api.jar";
 
+        ImageFromDockerfile imageFromDockerfile = new ImageFromDockerfile(imageName)
+                .withFileFromString("Dockerfile", buildDockerfile(imageName, jarFileName))
+                .withFileFromClasspath(jarFileName, jarFileName);
+
+        return imageFromDockerfile;
+    }
+
+    private static String buildDockerfile(String imageName, String jarFileName) {
         DockerfileBuilder builder = new DockerfileBuilder();
 
         builder
